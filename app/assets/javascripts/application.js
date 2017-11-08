2ba03f9833366a800579f5ccdbdc457e56286a0a
Matt Cheah <matt.cheah@gmail.com>
2017-11-08 03:36:26 +0000

    add window.onload to show alerts and notices if they have content in them

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,2 +1,14 @@
 //= require angular
 //= require_tree .
+
+window.onload = function() {
+    const alert = document.getElementById("alert");  
+    const notice = document.getElementById("notice");  
+
+    if (alert.innerHTML != "") {
+        alert.style.display = "block";
+    }
+    if (notice.innerHTML != "") {
+        notice.style.display = "block";
+    }
+};
