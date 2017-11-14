5497f9b969d096193963ff248a576ec7be110d7a
Matt Cheah <matt.cheah@gmail.com>
2017-11-14 09:54:52 +0000

    add flash notice partial and cause db seeds

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,16 +2,5 @@
 //= require rails-ujs
 //= require_tree .
 
-window.onload = function() {
-    const alert = document.getElementById("alert");  
-    const notice = document.getElementById("notice");  
 
-    if (alert.innerHTML != "") {
-        alert.style.display = "block";
-    }
-    if (notice.innerHTML != "") {
-        notice.style.display = "block";
-    }
-    
-};
 
--- app/models/cause.rb
+++ app/models/cause.rb
@@ -1,4 +1,4 @@
 class Cause < ApplicationRecord
-    has_many :causes, through: :user_causes
-    has_many :user_causes
+    has_many :causes, through: :users_causes
+    has_many :users_causes
 end
